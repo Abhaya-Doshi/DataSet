a8e921a256f6b496fef657c06d1573d2cfe5394c
Abhaya-Doshi <32240727+Abhaya-Doshi@users.noreply.github.com>
2017-10-02 21:29:16 +0530

    Adding DataSet DataRow Project
    A program comprising of data structure and utility function to perform basic operations offered by excel.

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author Abhaya Jayantilal Doshi
+ * Date: 2017.08.05(yyyy-mm-dd)
+ * Class Name: DataTypes
+ *
+ */
+
+package edu.projects.dataset;
+
+import java.io.Serializable;
+
+public enum DataTypes implements Serializable
+{
+	STRING,
+	BOOLEAN,
+	BYTE,
+	SHORT,
+	INTEGER,
+	LONG,
+	FLOAT,
+	DOUBLE;
+
+}
+
+
